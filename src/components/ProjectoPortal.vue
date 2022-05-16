dfbd26dda4cd1fc21efd2a578df09365da9d2bb7
Miguel Angel Castillo Colmenares <100499673+castillomiguel25@users.noreply.github.com>
2022-05-16 10:42:43 -0400

    update

---
+++ src/components/ProjectoPortal.vue
@@ -0,0 +1,148 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center mt-5 mb-5">
+        <h3>Completed Projects</h3>
+      </div>
+
+       <div class="col-12 col-md-6 text-center col-lg-3">
+        
+        <b-card 
+          bg-variant="dark"
+          text-variant="light"
+          title="Conference Hawai"
+          img-src="@/components/img/hawaii.jpg"
+          img-alt="Image"
+          img-height="300"
+          img-top
+          tag="article"
+          class=" mb-5"
+        >
+         
+            
+           <b-button
+          href="https://agenciadigitalcreandolo.netlify.app/"
+          class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button  href="https://github.com/castillomiguel25/agencydigital" class=" mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+
+       
+
+      <div class="col-12 col-md-6 text-center col-lg-3">
+        
+        <b-card 
+          bg-variant="dark"
+          text-variant="light"
+          title="Agencia Digital"
+          img-src="@/components/img/agencia-one.png"
+          img-alt="Image"
+          img-height="300"
+          img-top
+          tag="article"
+          class=" mb-5"
+        >
+         
+
+           <b-button
+          href="https://agenciadigitalcreandolo.netlify.app/"
+          class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button  href="https://github.com/castillomiguel25/agencydigital" class=" mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+
+      <div class="col-12 col-md-6 text-center col-lg-3">
+        <b-card
+         img-height="300"
+          bg-variant="dark"
+          text-variant="light"
+          title="CerrajerosR&E "
+          img-src="@/components/img/cerrajeria.png"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-5"
+        >
+            
+         
+
+          <b-button href="http://cerrajerosre.com/" class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button href="#" class="mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+
+      <div class="col-12 col-md-6 text-center col-lg-3">
+        <b-card
+         img-height="300"
+          bg-variant="dark"
+          text-variant="light"
+          title="Layetana"
+          img-src="@/components/img/layetana.png"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-5"
+        >
+          
+
+        <b-button href="https://layetanaproductions.com/" class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button href="#" class="mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+      <div class="col-12 col-md-6 text-center col-lg-3">
+        <b-card
+         img-height="300"
+          bg-variant="dark"
+          text-variant="light"
+          title="Platzi-Exchange"
+          img-src="@/components/img/tuexchange-two.png"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-5"
+        >
+         
+
+            <b-button href="https://tuexchangecrypto.netlify.app/" class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button href="https://github.com/castillomiguel25/Exchange-platzi" class="mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+       <div class="col-12 col-md-6 text-center col-lg-3">
+        <b-card
+         img-height="300"
+          bg-variant="dark"
+          text-variant="light"
+          title="Clone-Trello"
+          img-src="@/components/img/trello-logo.svg"
+          img-alt="Image"
+          img-top
+          tag="article"
+          class="mb-5"
+        >
+         
+
+            <b-button href="#" class="ml-3 mt-3 mr-5"
+          >View</b-button
+        >
+        <b-button href="https://github.com/castillomiguel25/clon-de-trello" class="mt-3" variant="primary"> Code</b-button>
+        </b-card>
+      </div>
+      
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
