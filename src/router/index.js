5b93bebcfc03dc1ca13e81c09c89bb6e5c24fa98
castillomiguel25 <miguelangelcastillo947@gmail.com>
2022-07-12 13:54:16 -0400

    'upa'

--- src/router/index.js
+++ src/router/index.js
@@ -1,38 +1,35 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import Router from 'vue-router'
 import HomeView from "@/views/HomeView.vue";
 import FormsPortal from "@/components/FormsPortal.vue";
 import PageFound from "@/components/PageFound.vue"
 
-Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: "/",
-    name: "Home-view",
-    component: HomeView,
-  },
 
-  {
-    path: "/forms",
-    name: "forms-portal",
-    component: FormsPortal,
-  },
+Vue.use(Router)
 
-  {
-    path: "/:pathMath(.*)",
+export default new Router({
+  mode: 'history',
+
+  routes: [
+    {
+      path: "/",
+      name: "Home-view",
+      component: HomeView,
+    },
+
+    {
+      path: "/forms",
+      name: "forms-portal",
+      component: FormsPortal,
+    },
+
+    
+
+    {
+      path: "/:pathMath(.*)",
     name: "PageFound",
     component: PageFound,
-  },
-
-  
-  
-]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    }
+  ]
 })
-
-export default router
