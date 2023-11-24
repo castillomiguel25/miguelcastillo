7b003a139b0f733d79c1fe733d47bddcc57a640c
castillomiguel25 <miguelangelcastillo947@gmail.com>
2023-11-24 15:07:28 -0400

    finih

--- src/components/HousePortal.vue
+++ src/components/HousePortal.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mx-auto max-w-screen-lg px-3 py-6">
     <h3 class="text-blue-400 text-2xl font-bold">{{ $t('message.greeting') }}👋</h3>
-    <h2 class="text-4xl font-bold">Miguel Castillo</h2>
+    <h1 class="text-4xl font-bold">Miguel Angel Castillo Colmenares</h1>
     <span class="ml-3 text-5xl text-red-600 font-bold">Frontend Developer</span>
     <p class="text-1xl text-gray-400 mb-5 mt-3">
       {{ $t('message.presentation') }}
